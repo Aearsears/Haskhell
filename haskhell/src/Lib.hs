380fadbe08465e2ef94896a69ff61c48602c78de
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-02-10 22:31:54 -0500

    haskhell ishard

--- haskhell/src/Lib.hs
+++ haskhell/src/Lib.hs
@@ -3,4 +3,27 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = 
+    do
+        txt <- getLine
+        printLine txt
+
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words  
+
+printLine:: String -> IO()
+printLine s = putStrLn s  
+
+-- printLines :: Num a ->IO()
+-- printLines 0 = return()
+-- printLines n = do
+--     putStrLn "yes"
+--     printLines n-1
+
+-- printLines :: IO()->IO()
+-- printLines 0 = return ()
+-- printLines n = do 
+--     printLine
+--     printLines n-1
+
+inside a = a `elem` [5,4,3,2,1]

02c07113f50fa31c3e4d85d9f0355a319c9bc9df
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-02-14 21:43:46 -0500

    progress on implementing concat function

--- src/Func.hs
+++ src/Func.hs
@@ -15,3 +15,24 @@ sayTimes x
     | x == 0    = "Done"
     | x == 2    = "almost"
     | otherwise = "hello"
+
+-- http://www.cs.uu.nl/docs/vakken/fp/2019/exercises.html
+-- function which takes in a list and returns the last element
+lastElem :: [a] -> a 
+lastElem [] = error "empty list"
+lastElem [a] = a 
+lastElem (h:t) = lastElem t 
+
+-- Define a function that determines whether a list is a palindrome, that is, whether the list is equal to its reversal.
+-- goog 
+palindrome :: (Eq a)=> [a] -> Bool
+palindrome l
+    | l == []               = True
+    | length l == 1             = True
+    | head l == lastElem l  = palindrome (tail (init l))
+    | otherwise             = False
+
+-- Define the function concat :: [[a]] −> [a] which flattens a list of lists: concat [[1, 2], [3], [ ], [4, 5]] evaluates to [1, 2, 3, 4, 5].
+concat :: [[a]] -> [a]
+concat [] = error "empty list"
+concat h:t = concat h:[t]
